f5379f7c435d025925115f29e27a2d709a0c3ca0
Mihaela Trempetić <32740446+mixy9@users.noreply.github.com>
2020-08-09 23:13:34 +0200

    design changes

--- js/main.js
+++ js/main.js
@@ -17,37 +17,39 @@ $(document).ready(function () {
 
     $nav = $('.nav');
     $toggleCollapse = $('.toggle-collapse');
-
-    /** click event on toggle menu */
+ 
     $toggleCollapse.click(function () {
         $nav.toggleClass('collapse');
     })
 
-    // owl-crousel
+    // Owl Carousel
     var owl = $('.owl-carousel');
     owl.owlCarousel({
-        items:4,
-        loop:true,
-        margin:10,
-        autoplay:true,
-        autoplayTimeout:2000,
-        autoplayHoverPause:true,  
+        items: 4,
+        loop: true,
+        margin: 10,
+        autoplay: true,
+        autoplayTimeout: 2000,
+        autoplayHoverPause: true,  
         autoplayTimeout: 3000,
         dots: true,
         nav: false,
         navText: [$('.owl-navigation .owl-nav-prev'), $('.owl-navigation .owl-nav-next')],
         responsive: responsive
     });
-
-
-    // click to scroll top
+ 
     $('.move-up span').click(function () {
         $('html, body').animate({
             scrollTop: 0
         }, 800);
     })
+ 
+    $('.move-archive').click(function () {
+        $('html, body').animate({ 
+            scrollTop: $("#archive").offset().top
+        }, 800);
+    })
 
-    // click to scroll bottom
     $('.move-down').click(function () {
         $('html, body').animate({
             scrollTop: $(document).height()
